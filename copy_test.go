40d7e6fabbb0632c7181a40fb4a9662ab30bb0ad
Jan Seidl <seidl@avast.com>
2017-04-24 23:16:57 +0200

    remove testing.Run, becuase is only in go 1.7+

--- copy_test.go
+++ copy_test.go
@@ -40,60 +40,55 @@ func TestTempFile(t *testing.T) {
 	assert.Exactly(t, true, temp.Exists(), "new temp file exists")
 }
 
-func TestCopyFile(t *testing.T) {
-	t.Run("dst file not exists",
-		func(t *testing.T) {
-			src, err := NewPath("copy_test.go")
-			assert.Nil(t, err)
+func TestCopyFileDstFileNotExists(t *testing.T) {
+	src, err := NewPath("copy_test.go")
+	assert.Nil(t, err)
 
-			dst, err := NewTempFile(TempFileOpt{})
-			assert.Nil(t, err)
-			defer dst.Remove()
-			dst.Remove()
+	dst, err := NewTempFile(TempFileOpt{})
+	assert.Nil(t, err)
+	defer dst.Remove()
+	dst.Remove()
 
-			assert.Exactly(t, false, dst.Exists(), "dst file isn't exists")
-			newDst, err := src.CopyFile(dst.String())
-			assert.Nil(t, err)
-			if err != nil {
-				t.Log(err)
-			} else {
-				assert.Equal(t, dst.String(), newDst.String(), "dst file is same as what return CopyFile")
-				assert.Exactly(t, true, newDst.Exists(), "dst file after copy exists")
-			}
-		})
+	assert.Exactly(t, false, dst.Exists(), "dst file isn't exists")
+	newDst, err := src.CopyFile(dst.String())
+	assert.Nil(t, err)
+	if err != nil {
+		t.Log(err)
+	} else {
+		assert.Equal(t, dst.String(), newDst.String(), "dst file is same as what return CopyFile")
+		assert.Exactly(t, true, newDst.Exists(), "dst file after copy exists")
+	}
+}
 
-	t.Run("dst file exists",
-		func(t *testing.T) {
-			src, _ := NewPath("copy_test.go")
-			dst, err := NewTempFile(TempFileOpt{})
-			assert.Nil(t, err)
-			defer dst.Remove()
+func TestCopyFileDstFileExists(t *testing.T) {
+	src, _ := NewPath("copy_test.go")
+	dst, err := NewTempFile(TempFileOpt{})
+	assert.Nil(t, err)
+	defer dst.Remove()
 
-			assert.Exactly(t, true, dst.Exists(), "dst file exists before copy")
-			newDst, err := src.CopyFile(dst.String())
-			if !assert.Nil(t, err) {
-				return
-			}
+	assert.Exactly(t, true, dst.Exists(), "dst file exists before copy")
+	newDst, err := src.CopyFile(dst.String())
+	if !assert.Nil(t, err) {
+		return
+	}
 
-			assert.Equal(t, dst.String(), newDst.String(), "dst file is same as what return CopyFile")
-			assert.Exactly(t, true, newDst.Exists(), "dst file after copy exists")
-		})
+	assert.Equal(t, dst.String(), newDst.String(), "dst file is same as what return CopyFile")
+	assert.Exactly(t, true, newDst.Exists(), "dst file after copy exists")
+}
 
-	t.Run("dst is dir",
-		func(t *testing.T) {
-			src, _ := NewPath("copy_test.go")
-			dst, _ := NewPath(os.TempDir())
+func TestCopyFileDstIsDir(t *testing.T) {
+	src, _ := NewPath("copy_test.go")
+	dst, _ := NewPath(os.TempDir())
 
-			assert.Exactly(t, true, dst.Exists(), "dst dir exists")
-			newDst, err := src.CopyFile(dst.String())
-			if !assert.Nil(t, err) {
-				return
-			}
+	assert.Exactly(t, true, dst.Exists(), "dst dir exists")
+	newDst, err := src.CopyFile(dst.String())
+	if !assert.Nil(t, err) {
+		return
+	}
 
-			defer newDst.Remove()
+	defer newDst.Remove()
 
-			assert.Exactly(t, true, newDst.Exists(), "dst after copy exists")
-			assert.Exactly(t, true, newDst.IsFile(), "dst is file")
-			assert.Equal(t, src.Basename(), newDst.Basename(), "if is dst directory, then is used basename of source")
-		})
+	assert.Exactly(t, true, newDst.Exists(), "dst after copy exists")
+	assert.Exactly(t, true, newDst.IsFile(), "dst is file")
+	assert.Equal(t, src.Basename(), newDst.Basename(), "if is dst directory, then is used basename of source")
 }
--- visit_test.go
+++ visit_test.go
@@ -5,47 +5,48 @@ import (
 	"testing"
 )
 
-func TestVisit(t *testing.T) {
+func TestVisitFlat(t *testing.T) {
+	path, err := NewPath("examples/tree")
+
+	assert.Nil(t, err)
+	flat_expected := map[string]int{
+		"examples/tree/4": 0,
+	}
+
+	flat := make(map[string]int)
+	path.Visit(
+		func(path *Path) {
+			flat[path.String()] = 0
+		},
+		VisitOpt{},
+	)
+
+	assert.Equal(t, flat_expected, flat, "flat files")
+}
+
+func TestVisitRecurse(t *testing.T) {
 	path, err := NewPath("examples/tree")
 
 	assert.Nil(t, err)
 
-	t.Run("flat visit", func(t *testing.T) {
-		flat_expected := map[string]int{
-			"examples/tree/4": 0,
-		}
+	flat_expected := map[string]int{
+		"examples/tree/a":     0,
+		"examples/tree/a/1":   0,
+		"examples/tree/a/b":   0,
+		"examples/tree/a/b/2": 0,
+		"examples/tree/c":     0,
+		"examples/tree/c/3":   0,
+		"examples/tree/4":     0,
+	}
 
-		flat := make(map[string]int)
-		path.Visit(
-			func(path *Path) {
-				flat[path.String()] = 0
-			},
-			VisitOpt{},
-		)
+	flat := make(map[string]int)
+	path.Visit(
+		func(path *Path) {
+			flat[path.String()] = 0
+		},
+		VisitOpt{Recurse: true},
+	)
 
-		assert.Equal(t, flat_expected, flat, "flat files")
-	})
-
-	t.Run("flat recurse", func(t *testing.T) {
-		flat_expected := map[string]int{
-			"examples/tree/a":     0,
-			"examples/tree/a/1":   0,
-			"examples/tree/a/b":   0,
-			"examples/tree/a/b/2": 0,
-			"examples/tree/c":     0,
-			"examples/tree/c/3":   0,
-			"examples/tree/4":     0,
-		}
-
-		flat := make(map[string]int)
-		path.Visit(
-			func(path *Path) {
-				flat[path.String()] = 0
-			},
-			VisitOpt{Recurse: true},
-		)
-
-		assert.Equal(t, flat_expected, flat, "flat files")
-	})
+	assert.Equal(t, flat_expected, flat, "flat files")
 
 }
