82623caa2f3c60e2d2113b253cb932a3ef17c0ea
Jan Seidl <seidl@avast.com>
2017-04-30 00:34:06 +0200

    add List methods

---
+++ open_test.go
@@ -0,0 +1,55 @@
+package pathutil
+
+import (
+	"bufio"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestOpenReader(t *testing.T) {
+	path, err := NewPath("open.go")
+
+	assert.Nil(t, err)
+
+	reader, file, err := path.OpenReader()
+	assert.Nil(t, err)
+	defer file.Close()
+
+	assert.IsType(t, new(bufio.Reader), reader)
+}
+
+func TestSlurp(t *testing.T) {
+	path, err := NewPath("./LICENSE")
+
+	assert.Nil(t, err)
+
+	ctx, err := path.Slurp()
+
+	assert.Equal(t, 1066, len(ctx), "read LICENSE file")
+}
+
+func TestLinesFunc(t *testing.T) {
+	path, err := NewPath("./LICENSE")
+
+	assert.Nil(t, err)
+
+	countOfLines := 0
+	linesFuncError := path.LinesFunc(func(line string) {
+		countOfLines++
+	})
+
+	assert.Nil(t, linesFuncError)
+	assert.Equal(t, 21, countOfLines)
+}
+
+func TestLines(t *testing.T) {
+	path, err := NewPath("./LICENSE")
+
+	assert.Nil(t, err)
+
+	lines, linesFuncError := path.Lines()
+
+	assert.Nil(t, linesFuncError)
+	assert.Equal(t, 21, len(lines))
+	assert.Equal(t, "MIT License", lines[0], "string without new line on end")
+}
